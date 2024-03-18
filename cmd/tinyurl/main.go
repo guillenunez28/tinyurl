48e7c5ab9469fc3c02c1cc451e7146da42769b5f
Guillermo Nunez <guillenunez28@yahoo.com>
2024-03-18 18:48:38 -0500

    Refactor main.go and implement API route handlers for tiny urls

--- cmd/tinyurl/main.go
+++ cmd/tinyurl/main.go
@@ -1,28 +1,45 @@
 package main
 
 import (
+	"database/sql"
+	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	tdb "github.com/tinyurl/internal/db"
 	"github.com/tinyurl/pkg/tinyurl"
 )
 
 func main() {
-	router := gin.Default()
+	db, err := tdb.InitDB()
+	if err != nil {
+		log.Fatal("Failed to initialize database:", err)
+	}
 
-	// Define routes for fetching tiny urls
-	router.GET("/", tinyurl.GetUrls)
-	router.GET("/:shortUrl", tinyurl.GetUrl)
-	router.GET("/:shortUrl/stats", tinyurl.GetUrlStats)
-
-	// Define routes for creating tiny urls
-	router.POST("/", tinyurl.PostUrl)
-
-	// Define routes for deleting tiny urls
-	router.DELETE("/:id", tinyurl.DeleteUrl)
+	defer db.Close()
+	router := defineRouter(db)
 
 	// Run the server
 	if err := http.ListenAndServe(":8080", router); err != nil {
 		panic(err)
 	}
+}
+
+func defineRouter(db *sql.DB) *gin.Engine {
+	router := gin.Default()
+
+	handler := tinyurl.Handler{
+		Db: db,
+	}
+	// Define routes for fetching tiny urls
+	router.GET("/", handler.GetUrls)
+	router.GET("/:shortUrl", handler.GetUrl)
+	router.GET("/:shortUrl/stats", handler.GetUrlStats)
+
+	// Define routes for creating tiny urls
+	router.POST("/", handler.PostUrl)
+
+	// Define routes for deleting tiny urls
+	router.DELETE("/:shortUrl", handler.DeleteUrl)
+	return router
 }
--- pkg/tinyurl/tinyurl.go
+++ pkg/tinyurl/tinyurl.go
@@ -1,6 +1,9 @@
 package tinyurl
 
 import (
+	"crypto/sha256"
+	"database/sql"
+	"encoding/hex"
 	"fmt"
 	"net/http"
 	"time"
@@ -8,71 +11,126 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type Handler struct {
+	Db *sql.DB
+}
+
 type TinyUrl struct {
-	ShortVersion    string    `json:"short_version"`
-	LongVersion     string    `json:"long_version"`
-	ExpirationTime  time.Time `json:"expiration_time"`
-	HitsLast24Hours int       `json:"hits_last_24_hours"`
-	HitsLastWeek    int       `json:"hits_last_week"`
-	HitsAllTime     int       `json:"hits_all_time"`
+	ShortUrl       string     `json:"short_url"`
+	LongUrl        string     `json:"long_url"`
+	ExpirationTime *time.Time `json:"expiration_date,omitempty"`
+}
+
+type Stats struct {
+	HitsLast24Hours int `json:"hits_last_24_hours,omitempty"`
+	HitsLastWeek    int `json:"hits_last_week,omitempty"`
+	HitsAllTime     int `json:"hits_all_time,omitempty"`
 }
 
 type PostTinyUrl struct {
-	LongVersion    string    `json:"long_version" binding:"required"`
-	ExpirationTime time.Time `json:"expiration_time"`
+	LongUrl        string `json:"long_url" binding:"required"`
+	ExpirationTime string `json:"expiration_date,omit_empty"`
+	ShortUrl       string
 }
 
 // GetUrls fetches the existing resources for a given user.
 // For now this returns nothing.
-func GetUrls(c *gin.Context) {
-	// TODO Add DB handler to fetch the resources
-	c.IndentedJSON(http.StatusOK, []string{})
+func (h *Handler) GetUrls(c *gin.Context) {
+	urls, err := getDbResources(h.Db)
+	if err != nil {
+		c.IndentedJSON(http.StatusBadRequest, "error fetching data from DB")
+	}
+	c.IndentedJSON(http.StatusOK, urls)
 }
 
 // GetUrl fetches the tiny url resource based on the unique indentifier
 // and redirects you to the long url. For now, it just returns some mock data.
-func GetUrl(c *gin.Context) {
-	// TODO Add DB handler to fetch the resource
-	shortUrl := c.Query("shortUrl")
-	// TODO Add DB handler to fetch the resource
-	resource := TinyUrl{
-		ShortVersion:   shortUrl,
-		LongVersion:    "https://www.google.com",
-		ExpirationTime: time.Now(),
+func (h *Handler) GetUrl(c *gin.Context) {
+	shortUrl := c.Param("shortUrl")
+	resource, err := getDbResource(h.Db, shortUrl)
+	if err != nil {
+		c.IndentedJSON(http.StatusBadRequest, err.Error())
+		return
 	}
-	http.Redirect(c.Writer, c.Request, resource.LongVersion, http.StatusFound)
+	err = addDbUrlHit(h.Db, shortUrl)
+	if err != nil {
+		c.IndentedJSON(http.StatusBadRequest, err.Error())
+		return
+	}
+	redirectLink := "https://" + resource.LongUrl
+	http.Redirect(c.Writer, c.Request, redirectLink, http.StatusFound)
 }
 
 // GetUrlStats fetches the tiny url resource based on the unique indentifier.
 // For now, it just returns some mock data.
-func GetUrlStats(c *gin.Context) {
-	// TODO Add DB handler to fetch the resource
-	shortUrl := c.Query("shortUrl")
-	// TODO Add DB handler to fetch the resource
-	resource := TinyUrl{
-		ShortVersion:    shortUrl,
-		LongVersion:     "www.google.com",
-		ExpirationTime:  time.Now(),
-		HitsAllTime:     4,
-		HitsLast24Hours: 1,
-		HitsLastWeek:    2,
+func (h *Handler) GetUrlStats(c *gin.Context) {
+	shortUrl := c.Param("shortUrl")
+	resource, err := getDbResourceStats(h.Db, shortUrl)
+	if err != nil {
+		c.IndentedJSON(http.StatusBadRequest, err.Error())
+		return
 	}
 	c.IndentedJSON(http.StatusOK, resource)
 }
 
 // PostUrl creates a tiny url resource. The tiny url short version must be unique.
-func PostUrl(c *gin.Context) {
+func (h *Handler) PostUrl(c *gin.Context) {
 	var tinyUrl PostTinyUrl
 	if err := c.BindJSON(&tinyUrl); err != nil {
 		c.IndentedJSON(http.StatusBadRequest, "Bad Payload")
 		return
 	}
-	c.IndentedJSON(http.StatusOK, nil)
+	x := TinyUrl{
+		LongUrl: tinyUrl.LongUrl,
+	}
+	if tinyUrl.ExpirationTime != "" {
+		timeExpired, err := time.Parse("2006-01-02", tinyUrl.ExpirationTime)
+		if err != nil {
+			c.IndentedJSON(http.StatusBadRequest, "Bad timestamp")
+			return
+		}
+		x.ExpirationTime = &timeExpired
+	}
+	x.ShortUrl = generateShortURL(tinyUrl.LongUrl)
+
+	err := createDbResource(h.Db, x)
+	if err != nil {
+		c.IndentedJSON(http.StatusBadRequest, "DB error")
+		return
+	}
+	c.IndentedJSON(http.StatusOK, x)
 }
 
 // DeleteUrl deletes the tiny url resource.
-func DeleteUrl(c *gin.Context) {
-	id := c.Query("id")
+func (h *Handler) DeleteUrl(c *gin.Context) {
+	shortUrl := c.Param("shortUrl")
+	err := deleteDbResource(h.Db, shortUrl)
+	if err != nil {
+		c.IndentedJSON(http.StatusBadRequest, err.Error())
+		return
+	}
 	// TODO Add DB handler to delete resource
-	c.IndentedJSON(http.StatusOK, fmt.Sprintf("Resource with ID %s deleted", id))
+	c.IndentedJSON(http.StatusOK, fmt.Sprintf("Resource with ID %s deleted", shortUrl))
+}
+
+const (
+	lengOfUrl = 6
+)
+
+// generateShortUrl creates a short URL based on the long URL provided and
+// the current time string. One limitation is that it may return all
+// digits.
+func generateShortURL(longURL string) string {
+	// Append the current timestamp to the long URL to make a random string.
+	input := longURL + time.Now().String()
+	hash := sha256.New()
+
+	hash.Write([]byte(input))
+	hashBytes := hash.Sum(nil)
+	hashString := hex.EncodeToString(hashBytes)
+
+	// Take a substring of the hash string to get the random string
+	randomString := hashString[:lengOfUrl]
+
+	return randomString
 }
